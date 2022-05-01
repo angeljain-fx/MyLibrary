1d55a2b18dfe9222732745fecdf3668696aac382
Angel Jain <angeljain.fx@gmail.com>
2022-05-01 18:41:48 +0530

    Author Routing Completed

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
@@ -26,5 +29,6 @@ db.on('error', error => console.log(error));
 db.once('open', () => console.log("Connected to mongoose"));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
