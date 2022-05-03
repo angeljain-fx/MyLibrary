73d474f14f1935f6ad0a3083d795664a11b30575
Angel Jain <angeljain.fx@gmail.com>
2022-05-03 13:28:48 +0530

    backend stage 1 complete

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -18,6 +19,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
